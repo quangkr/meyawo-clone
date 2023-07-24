01fe53ec5b938c135644ce539dba28211d226f2c
Quang KR <dangminhquang.r@gmail.com>
2023-07-24 08:10:02 +0700

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true
-    })
-  ]
+  integrations: [UnoCSS({
+    injectReset: true
+  }), preact()]
 });
