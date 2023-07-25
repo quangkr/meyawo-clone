4f3c2a06624160e20b89f7ed00d4b6d0ea2b9ed8
Quang KR <dangminhquang.r@gmail.com>
2023-07-25 14:25:42 +0700

    Add NavBar component

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
         mono: 'Fira Code',
         baloo: [
           {
-            name: 'Baloo 2'
+            name: 'Baloo 2',
+            weights: ['400', '700', '900']
           },
           {
             name: 'sans-serif',
@@ -26,6 +27,12 @@ export default defineConfig({
       primary: {
         500: 'hsla(var(--hue), 62%, 62%)'
       }
+    },
+    breakpoints: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
     }
   }
 });
